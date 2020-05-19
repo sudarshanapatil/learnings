9ba5af11746f856007dda0190f6a7118ac7d9243
sudarshana <patilsudarshana94@gmail.com>
2020-05-19 15:21:20 +0530

    Create callback.js

---
+++ callback.js
@@ -0,0 +1,23 @@
+function fun1(arr, cb) {
+    filterArr = arr.filter((data) => (data % 2 === 0))
+    cb(filterArr)
+}
+
+function fun2(arr, cb) {
+    sum = arr.map(((item) => item + 1))
+    cb(sum)
+}
+let arr = [0, 2, 4, 3, 5]
+function demo(arr) {
+    fun1(arr, (evenArr) => {
+        fun2(evenArr, (res) => { console.log(res) })
+    })
+
+    fun2(arr, (filterArr) => {
+        fun1(filterArr, (res) => { console.log(res) })
+    })
+}
+
+demo(arr)
+
+
