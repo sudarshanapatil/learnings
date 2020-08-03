6bfc35385e057fc82a4fc9c6cd2a465421f073ae
sudarshana <patilsudarshana94@gmail.com>
2020-08-04 00:18:40 +0530

    Update balancedParentheses.js

--- balancedParentheses.js
+++ balancedParentheses.js
@@ -1,30 +1,25 @@
-
-
 function test(testString) {
     let obj = {
         "{": "}",
         "[": "]",
         "(": ")"
     }
-    let valid = true;
-    if (!testString.length % 2) {  //check only if string length is even
-        for (let i = 0; i <= testString.length / 2 - 1; i++) {
-            let startIndex = testString.charAt(i);
-            let lastIndex = testString.charAt(testString.length - 1 - i);
-            if (obj[startIndex] !== lastIndex) {
-                valid = false;
-                break;
+    let stack = [];
+    if (!(testString.length % 2)) {  //check only if string length is even
+        for (let i = 0; i < testString.length; i++) {
+            if (testString[i] === "{" || testString[i] === "(" || testString[i] === "[")
+                stack.push(testString[i]);
+            else {
+                if (testString[i] === obj[stack[stack.length - 1]])
+                    stack.pop();
             }
         }
+        if (stack.length === 0)
+            console.log("corrrect parantheses");
+        else
+            console.log("incorrrect parantheses");
     }
-    else  //if odd length invalid 
-        valid = false;
-
-    if (valid)
-        console.log("corrrect paranthesis");
     else
-        console.log("incorrrect paranthesis");
+        console.log("incorrrect parantheses");
 }
-test("[{]");
-
-
+test("[]");
